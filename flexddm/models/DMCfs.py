fix wiener process formula

flexddm/models/DMCfs.py
@@ -94,7 +94,7 @@ class DMCfs(Model):
                 else:
                     delta = (-peak_amplitude * np.exp(-(t / characteristic_time)) *
                             np.power(((t * np.exp(1)) / ((shape - 1) * characteristic_time)), (shape - 1)) * (((shape - 1) / t) - (1 / characteristic_time))) + mu_c
-                evidence += delta*dt + np.random.choice(update_jitter)
+                evidence += delta*dt + np.random.choice(update_jitter) * np.sqrt(dt)
                 t += dt # increment time by the unit dt
                 if evidence > alpha/2:
                     choicelist[n] = 1

flexddm/models/DSTP.py
@@ -95,8 +95,8 @@ class DSTP(Model):
             evidenceRS1 = betaRS*alphaRS - (1-betaRS)*alphaRS
             np.random.seed(n)
             while (evidenceSS < alphaSS/2 and evidenceSS > -alphaSS/2) or (evidenceRS1 < alphaRS/2 and evidenceRS1 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                evidenceSS += deltaSS*dt + np.random.choice(update_jitter) # add one of the many possible updates to evidence
-                evidenceRS1 += deltaRS1*dt + np.random.choice(update_jitter)
+                evidenceSS += deltaSS*dt + np.random.choice(update_jitter) * np.sqrt(dt) # add one of the many possible updates to evidence
+                evidenceRS1 += deltaRS1*dt + np.random.choice(update_jitter) * np.sqrt(dt)
                 t += dt # increment time by the unit dt
             if evidenceRS1 > alphaRS/2:
                 choicelist[n] = 1 # choose the upper threshold action
@@ -113,7 +113,7 @@ class DSTP(Model):
                     deltaRS = -1 * deltaRS
                 evidenceRS2 = evidenceRS1 # start where you left off from RS1
                 while (evidenceRS2 < alphaRS/2 and evidenceRS2 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                    evidenceRS2 += deltaRS*dt + np.random.choice(update_jitter)
+                    evidenceRS2 += deltaRS*dt + np.random.choice(update_jitter) * np.sqrt(dt)
                     t += dt # increment time by the unit dt
                 if evidenceRS2 > alphaRS/2:
                     choicelist[n] = 1 # choose the upper threshold action

flexddm/models/SSPit.py
@@ -91,7 +91,7 @@ class SSPit(Model):
                     delta = s_ta*p - s_fl*p
                 else:
                     delta = s_ta*p + s_fl*p
-                evidence += (delta*dt + np.random.choice(noise)) # add one of the many possible updates to evidence
+                evidence += (delta*dt + np.random.choice(noise) * np.sqrt(dt)) # add one of the many possible updates to evidence
                 t += dt 
             if evidence > alpha/2:
                 choicelist[n] = 1 # choose the upper threshold action

flexddm/models/StandardDDM.py
@@ -86,7 +86,7 @@ class StandardDDM(Model):
             evidence = beta*alpha - (1-beta)*alpha # start our evidence at initial-bias beta
             np.random.seed(n)
             while evidence < alpha/2 and evidence > -alpha/2: # keep accumulating evidence until you reach a threshold
-                evidence += delta*dt + np.random.choice(updates) # add one of the many possible updates to evidence
+                evidence += delta*dt + np.random.choice(updates) * np.sqrt(dt) # add one of the many possible updates to evidence
                 t += dt # increment time by the unit dt
             if evidence > alpha:
                 choicelist[n] = 1 # choose the upper threshold action

flexddm/models/mDMCfs.py
@@ -97,7 +97,7 @@ class mDMCfs(Model):
                 else:
                     delta = (-peak_amplitude * np.exp(-(t / characteristic_time)) *
                             np.power(((t * np.exp(1)) / ((shape - 1) * characteristic_time)), (shape - 1)) * (((shape - 1) / t) - (1 / characteristic_time))) + mu_c
-                delta_noise = np.random.choice(update_jitter)
+                delta_noise = np.random.choice(update_jitter) * np.sqrt(dt)
                 delta_noise = delta_noise*(np.exp(-1*(eta_r/2)*((t-tau))))
                 
                 evidence += delta*dt + delta_noise

flexddm/models/mDSTP.py
@@ -104,8 +104,8 @@ class mDSTP(Model):
             evidenceRS1 = betaRS*alphaRS - (1-betaRS)*alphaRS
             np.random.seed(n)
             while (evidenceSS < alphaSS/2 and evidenceSS > -alphaSS/2) or (evidenceRS1 < alphaRS/2 and evidenceRS1 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                delta_noise_SS = np.random.choice(update_jitter_SS)
-                delta_noise_RS1 = np.random.choice(update_jitter_RS1)
+                delta_noise_SS = np.random.choice(update_jitter_SS) * np.sqrt(dt)
+                delta_noise_RS1 = np.random.choice(update_jitter_RS1) * np.sqrt(dt)
                 delta_noise_SS = delta_noise_SS*(np.exp(-1*(eta_r/2)*((t-tau))))
                 delta_noise_RS1 = delta_noise_RS1*(np.exp(-1*(eta_r/2)*((t-tau))))
                 evidenceSS += deltaSS*dt + delta_noise_SS # add one of the many possible updates to evidence
@@ -126,7 +126,7 @@ class mDSTP(Model):
                     deltaRS = -1 * deltaRS
                 evidenceRS2 = evidenceRS1 # start where you left off from RS1
                 while (evidenceRS2 < alphaRS/2 and evidenceRS2 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                    delta_noise_RS2 = np.random.choice(update_jitter_RS2)
+                    delta_noise_RS2 = np.random.choice(update_jitter_RS2) * np.sqrt(dt)
                     delta_noise_RS2 = delta_noise_RS2*(np.exp(-1*(eta_r/2)*((t-tau))))
                     evidenceRS2 += deltaRS*dt + delta_noise_RS2
                     t += dt # increment time by the unit dt

flexddm/models/mSSP.py
@@ -96,7 +96,7 @@ class mSSP(Model):
                     sd = 0.001
                 s_ta = ((1 + math.erf((.5 - 0) / sd / np.sqrt(2))) / 2) - ((1 + math.erf((-.5 - 0) / sd / np.sqrt(2))) / 2)
                 s_fl = 1 - s_ta
-                delta_noise = np.random.choice(noise)
+                delta_noise = np.random.choice(noise) * np.sqrt(dt)
                 delta_noise = delta_noise*(np.exp(-1*(eta_r/2)*((t-tau))))
                 if congruencylist[n] == 'incongruent':
                     delta = s_ta*p - s_fl*p